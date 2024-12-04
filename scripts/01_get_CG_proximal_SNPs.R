1e16ea69219d43c1af6dc95e9127b70d12c18e40
hrayjones <rayjoneshelen@gmail.com>
2024-12-04 14:43:47 +0100

    Add a sample of the GWAS catalog file

---
+++ scripts/01_get_CG_proximal_SNPs.R
@@ -0,0 +1,6 @@
+### The manifest files show SNPs within 100?bp of the CpG site.
+### We can compare these SNPs against GWAS genome-wide significant SNPS using the GWAS catalog.
+### These are rsids.
+### 1) Compare the number of GWAS SNPs in MSA versus EPIC.
+### 2) Look at GWAS trait ontologies in each.
+
